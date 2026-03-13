e3f1e8adedc44b2aee7a6d003f004263e330c9cb
Kenneth Davidson <xylene2016@gmail.com>
2026-03-13 15:50:10 -0400

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+TZ='UTC'; export TZ
+umask 022
+set -euo pipefail
+
+cd /usr/local
+wget https://github.com/icebluey/ziprust/releases/download/v1.94.0/rust-v1.94.0-stable-x86_64-ub2204.tar.xz
+tar -xof rust-v1.94.0-stable-x86_64-ub2204.tar.xz
+sleep 1
+rm -f rust*.tar*
+. /usr/local/rust/env
+
+_tmp_dir="$(mktemp -d)"
+cd "${_tmp_dir}"
+
+wget https://github.com/openai/codex/archive/refs/tags/rust-v0.114.0.tar.gz
+tar -xof rust-*.tar*
+sleep 1
+rm -f rust-*.tar*
+cd codex*
+sed 's/pub const DEFAULT_ORIGINATOR: &str = "codex_cli_rs"/pub const DEFAULT_ORIGINATOR: &str = "Codex Desktop"/g' -i codex-rs/core/src/default_client.rs
+cd codex-rs
+cargo check
+cargo build --release -p codex-cli --bin codex
+ls -la
+echo ' done'
+exit
+
+
+cd /tmp
+rm -fr "${_tmp_dir}"
